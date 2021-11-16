84a47c94c311fa9d42e0788c7f7a2b2b3683bbb7
susmit31 <gohanmysticdbz@gmail.com>
2021-11-16 23:41:56 +0600

    minor

--- gravity_collision_demo.py
+++ gravity_collision_demo.py
@@ -6,7 +6,7 @@ HEIGHT = int(sys.argv[2])
 
 sc = Scene2D(WIDTH, HEIGHT)
 
-def rungame(input_handler, argdict, FPS=32):
+def rungame(input_handler, argdict, FPS=5):
     run_input_thread(input_handler, argdict)
     while 1:
         if argdict['quitGame']:
@@ -32,9 +32,9 @@ def handle_input(argdict):
 r1 = Rect(sc, (3,3), WIDTH//3, 2)
 r2 = Rect(sc, (r1.max_x()-5, r1.max_y()+5), WIDTH//2, 2)
 r3 = Rect(sc, (r2.max_x()-5, r2.max_y()+5), WIDTH//3, 2)
-r4 = Rect(sc, (r3.max_x()-8, r3.min_y()-6), 4, 6)
+# r4 = Rect(sc, (r3.max_x()-8, r3.min_y()-6), 4, 6)
 r5 = Rect(sc, (sc.width-7, sc.height-2), 4, 2)
-for r in (r1, r2, r3, r4, r5):
+for r in (r1, r2, r3, r5):
     sc.add_ground(r)
 
 box = Rect(sc, (r1.min_x() + 2, r1.min_y() -2), 2, 2, 'cyan')
